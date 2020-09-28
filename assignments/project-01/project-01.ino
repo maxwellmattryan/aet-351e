55959b3eaaf50df2bae81fc602a87d0a2e66fe4d
maxwellmattryan <maxwellmattryan@gmail.com>
2020-09-27 21:53:19 -0500

    Add randomization to led sequencing

--- assignments/project-01/project-01.ino
+++ assignments/project-01/project-01.ino
@@ -1,12 +1,15 @@
 int ledPins[] = {1, 3, 5, 7, 9};
 int ledPinNum = 5;
+int idleLedPin = 2;
 
-int potPin = A0;
+int randomnessPotPin = A0;
+int ratePotPin = A1;
 
-int maxDelayTime = 100;
+int minDelayTime = 5;
+int maxDelayTime = 250;
 
 // SETUP
-void setup() {
+void setup() { 
   Serial.begin(9600);
 
   initPinModes();
@@ -24,15 +27,22 @@ void loop() {
 }
 
 void writeLedPins() {
-  for(int i = 0; i < ledPinNum; i++) {
-    digitalWrite(ledPins[i], HIGH);
-    delay(readDelayTime());
-    digitalWrite(ledPins[i], LOW);
+  if(analogRead(ratePotPin) <= 1) {
+    digitalWrite(ledPins[idleLedPin], HIGH);
+  } else {
+    digitalWrite(ledPins[idleLedPin], LOW);
+    
+    for(int i = 0; i < ledPinNum; i++) {
+      int index = random(1024) < analogRead(randomnessPotPin) ? random(ledPinNum) : i;
+      
+      digitalWrite(ledPins[index], HIGH);
+      delay(readDelayTime());
+      digitalWrite(ledPins[index], LOW);
+    }
   }
 }
 
 int readDelayTime() {
-  float potValue = 1.0f - analogRead(potPin) / 1023.0f;
-  Serial.println(potValue, maxDelayTime * potValue);
-  return maxDelayTime * potValue;
+  float potValue = 1.0f - analogRead(ratePotPin) / 1023.0f;
+  return minDelayTime + (maxDelayTime - minDelayTime) * potValue;
 }
